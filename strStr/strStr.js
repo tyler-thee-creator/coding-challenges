6bee925a78b5237dd485c4055b4df2a230075953
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-06-30 03:20:00 -0700

    Complete strStr CC

---
+++ strStr/strStr.js
@@ -0,0 +1,30 @@
+/*https://leetcode.com/problems/implement-strstr/*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+const strStr = (haystack, needle) => {
+  if (needle === '') {
+      return 0;
+  }
+
+  for (let i = 0; i < haystack.length; i++) {
+      if (haystack[i] === needle[0]) {
+          let match = true;
+          for (let x = 1; x < needle.length; x++) {
+              if (needle[x] !== haystack[i + x]) {
+                  match = false;
+                  break;
+              }
+          }
+          if (match) {
+              return i;
+          }
+      }
+  }
+
+  return -1;
+}

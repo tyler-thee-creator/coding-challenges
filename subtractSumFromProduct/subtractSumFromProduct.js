36c82efe4bec53385afc6c4763899baf806519c4
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-01 21:56:46 -0700

    Complete subtractSumFromProduct CC

--- subtractSumFromProduct/subtractSumFromProduct.js
+++ subtractSumFromProduct/subtractSumFromProduct.js
@@ -5,8 +5,17 @@
  * @return {number}
  */
 
-const subtractProductAndSum = (n) => {
-  // code
+ const subtractProductAndSum = (n) => {
+  const numToArr = n.toString().split('');
+  let product = 1;
+  let sum = 0;
+
+  numToArr.map((num) => {
+    product *= (num - 0);
+    sum += (num - 0);
+  });
+
+  return product - sum;
 }
 
 export default subtractProductAndSum;

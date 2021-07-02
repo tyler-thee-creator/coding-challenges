4be84fc3ad16358f2188a9f1f43a675f00a53ea6
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-01 21:31:02 -0700

    Complete flippingAnImage CC

--- flippingAnImage/flippingAnImage.js
+++ flippingAnImage/flippingAnImage.js
@@ -5,6 +5,20 @@
  * @return {number[][]}
  */
 
-const flipAndInvertImage = (image) => {
-  // code
+ const flipAndInvertImage = (image) => {
+  const flippedImage = [];
+  const opposite = {
+    0: 1,
+    1: 0,
+  };
+
+  image.map((imageRow) => {
+    const flippedRow = [];
+    for (let i = imageRow.length - 1; i >= 0; i--) {
+      flippedRow.push(opposite[imageRow[i]]);
+    }
+    flippedImage.push(flippedRow);
+  });
+
+  return flippedImage;
 }

af9505ee4e61da4a4334464f5d68d6b7bca08c15
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-03 14:56:01 -0700

    Initialize combinations CC

---
+++ combinations/combinations.js
@@ -0,0 +1,13 @@
+/*https://leetcode.com/problems/combinations*/
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+
+const combine = (n, k) => {
+  // code
+}
+
+export default combine;

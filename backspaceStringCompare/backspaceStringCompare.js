5ccac557c8ecf33ecb94bb88119044d7fc62debf
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-03 19:39:58 -0700

    Complete backspaceStringCompare CC

--- backspaceStringCompare/backspaceStringCompare.js
+++ backspaceStringCompare/backspaceStringCompare.js
@@ -6,8 +6,31 @@
  * @return {boolean}
  */
 
-const backspaceCompare = (s, t) => {
+ const backspaceCompare = (s, t) => {
+  if (s == t) {
+    return true;
+  }
 
+  let sString = '';
+  let tString = '';
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== '#') {
+      sString += s[i];
+    } else {
+      sString = sString.substring(0, sString.length - 1);
+    }
+  }
+
+  for (let x = 0; x < t.length; x++) {
+    if (t[x] !== '#') {
+      tString += t[x];
+    } else {
+      tString = tString.substring(0, tString.length - 1);
+    }
+  }
+  console.log(sString, tString);
+  return sString === tString;
 }
 
 export default backspaceCompare;

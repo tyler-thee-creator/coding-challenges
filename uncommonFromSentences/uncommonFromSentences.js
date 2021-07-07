c9e3c0a5be0b053c25b6a6b55877d3975dc2a6f1
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-06 21:00:14 -0700

    Complete uncommonFromSentences CC

--- uncommonFromSentences/uncommonFromSentences.js
+++ uncommonFromSentences/uncommonFromSentences.js
@@ -6,8 +6,28 @@
  * @return {string[]}
  */
 
-const uncommonFromSentences = (s1, s2) => {
-  // code
+ const uncommonFromSentences = (s1, s2) => {
+  const uncommonWords = [];
+  const s1ToArray = s1.split(' ');
+  const s2ToArray = s2.split(' ');
+
+  s1ToArray.map((word, i) => {
+    s1ToArray[i] = null;
+    if (!(s1ToArray.indexOf(word) + 1) && !(s2ToArray.indexOf(word) + 1)) {
+      uncommonWords.push(word);
+    }
+    s1ToArray[i] = word;
+  });
+
+  s2ToArray.map((word, x) => {
+    s2ToArray[x] = null;
+    if (!(s1ToArray.indexOf(word) + 1) && !(s2ToArray.indexOf(word) + 1)) {
+      uncommonWords.push(word);
+    }
+    s2ToArray[x] = word;
+  });
+
+  return uncommonWords;
 }
 
 export default uncommonFromSentences

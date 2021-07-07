d5f26339b9c558b76c9b7ff66bc836a82cc4135e
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-06 20:20:03 -0700

    Initialize uncommonFromSentences CC

---
+++ uncommonFromSentences/uncommonFromSentences.js
@@ -0,0 +1,13 @@
+/*https://leetcode.com/problems/uncommon-words-from-two-sentences/*/
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
+ */
+
+const uncommonFromSentences = (s1, s2) => {
+  // code
+}
+
+export default uncommonFromSentences

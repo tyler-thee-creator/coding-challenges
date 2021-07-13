b2a8f19d5909796ccf433a720fcd8b445db58587
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-12 22:27:23 -0700

    Iteration 1

--- wildcardMatching/wildcardMatching.js
+++ wildcardMatching/wildcardMatching.js
@@ -6,12 +6,37 @@
  * @return {boolean}
  */
 
- const isMatch = (s, p) => {
+const isMatch = (s, p) => {
   if (p === '*') {
     return true;
   } else if (!(p.indexOf('?') + 1) && !(p.indexOf('*') + 1)) {
     return s === p;
   }
+
+  const wcSplit = p.split('*');
+  const sCopy = s;
+
+  wcSplit.map((phrase) => {
+    const q = phrase.indexOf('?') + 1;
+    if (q) {
+      const subPhrases = phrase.split('?');
+      const positions = [];
+
+      subPhrases.map((sp) => {
+        const start = s.indexOf(sp);
+
+        if (!(start + 1)) {
+          return false;
+        } else {
+          const posObj = {};
+          posObj[sp] = { start: start, end: start + sp.length };
+          positions.push(posObj);
+        }
+      });
+
+      console.log(positions);
+    }
+  });
 }
 
 export default isMatch;

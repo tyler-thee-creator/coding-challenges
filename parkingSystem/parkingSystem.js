413161d25256df51feaa24466dba1bc596da7532
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-23 11:55:55 -0700

    Futher initialize parkingSystem CC

---
+++ parkingSystem/parkingSystem.js
@@ -0,0 +1,27 @@
+/*https://leetcode.com/problems/design-parking-system/*/
+
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+
+const ParkingSystem = (big, medium, small) => {
+  // code
+};
+
+/**
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = (carType) => {
+  // code
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
+
+export default ParkingSystem;

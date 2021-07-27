fb5ddd45549bc08f09c2b5fb606c8afaffacec3a
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-26 18:18:43 -0700

    Complete parkingSystem CC

--- parkingSystem/parkingSystem.js
+++ parkingSystem/parkingSystem.js
@@ -6,22 +6,35 @@
  * @param {number} small
  */
 
-const ParkingSystem = (big, medium, small) => {
-  // code
+const ParkingSystem = function(big, medium, small) {
+  this.bigSlots = big;
+  this.mediumSlots = medium;
+  this.smallSlots = small;
 };
 
 /**
- * @param {number} carType
- * @return {boolean}
- */
-ParkingSystem.prototype.addCar = (carType) => {
-  // code
+* @param {number} carType
+* @return {boolean}
+*/
+ParkingSystem.prototype.addCar = function(carType) {
+  const spotTypes = {
+      1: 'bigSlots',
+      2: 'mediumSlots',
+      3: 'smallSlots',
+  };
+
+  if (this[spotTypes[carType]]) {
+      this[spotTypes[carType]]--;
+      return true;
+  } else {
+      return false;
+  }
 };
 
 /**
- * Your ParkingSystem object will be instantiated and called as such:
- * var obj = new ParkingSystem(big, medium, small)
- * var param_1 = obj.addCar(carType)
- */
+* Your ParkingSystem object will be instantiated and called as such:
+* var obj = new ParkingSystem(big, medium, small)
+* var param_1 = obj.addCar(carType)
+*/
 
 export default ParkingSystem;

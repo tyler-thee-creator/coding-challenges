8a110ff4f6e4974203bf8c5ee77547d2549d1e8c
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-27 18:44:56 -0700

    Initialize sumClosest CC

---
+++ sumClosest/sumClosest.js
@@ -0,0 +1,13 @@
+/*https://leetcode.com/problems/3sum-closest/*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+const threeSumClosest = (nums, target) => {
+  // code
+}
+
+export default threeSumClosest;

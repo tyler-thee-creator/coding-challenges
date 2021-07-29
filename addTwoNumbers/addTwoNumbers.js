530b7cbf1d1770df73f602b89b68168c11a56d59
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-07-28 21:32:57 -0700

    Complete addTwoNumbers CC

--- addTwoNumbers/addTwoNumbers.js
+++ addTwoNumbers/addTwoNumbers.js
@@ -14,7 +14,42 @@
  */
 
 const addTwoNumbers = (l1, l2) => {
-  // code
+  let l1Copy = l1;
+  let l2Copy = l2;
+  let l1NumString = '';
+  let l2NumString = '';
+
+  while (l1Copy !== null || l2Copy !== null) {
+    if (l1Copy !== null) {
+        l1NumString += l1Copy.val;
+        l1Copy = l1Copy.next;
+    }
+
+    if (l2Copy !== null) {
+        l2NumString += l2Copy.val;
+        l2Copy = l2Copy.next;
+    }
+  }
+
+  l1NumString = l1NumString.split('').reverse().join('');
+  l2NumString = l2NumString.split('').reverse().join('');
+  let sum = BigInt(l1NumString) + BigInt(l2NumString);
+  sum = sum.toLocaleString('fullwide', { useGrouping: false });
+
+  let head;
+  let current;
+
+  for (let i = sum.length - 1; i >= 0; i--) {
+    if (head === undefined) {
+      head = new ListNode(sum[i]);
+      current = head;
+    } else {
+      current.next = new ListNode(sum[i]);
+      current = current.next;
+    }
+  }
+
+  return head;
 }
 
 export default addTwoNumbers;
